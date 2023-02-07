35bd8cb91d56d03899732e6222c5cff1668c1d28
usual <usual@DESKTOP-NS5CSDE>
2023-02-07 16:49:26 +0100

    Add project files.

---
+++ Branches/Branch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branches
+{
+    internal class Branch
+    {
+        List<Branch> branches = new List<Branch>();
+
+        public Branch() {
+        }
+        
+        public void Add(Branch branch)
+        {
+            this.branches.Add(branch);
+        }
+
+        public int GetDepth(Branch branch)
+        {
+            int maxlevel = 0;
+            foreach (Branch childBranch in branch.branches) {
+                maxlevel = Math.Max(maxlevel, this.GetDepth(childBranch));
+            }
+            maxlevel += 1;
+            return maxlevel;
+        }
+
+        public string Show()
+        {
+            return $"Depth of branches structure is {this.GetDepth(this)}";
+        }
+    }
+}
+
---
+++ Branches/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Branches;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        Branch root = new Branch();
+        //LEFT TREE
+        Branch branch = new Branch();
+        root.Add(branch);
+
+        Branch branch2 = new Branch();
+        branch.Add(branch2);
+        //RIGHT TREE
+        Branch branch3 = new Branch();
+        root.Add(branch3);
+
+        Branch branch4 = new Branch();
+        branch3.Add(branch4);
+        Branch branch5 = new Branch();
+        branch3.Add(branch5);
+        Branch branch6 = new Branch();
+        branch3.Add(branch6);
+        Branch branch7 = new Branch();
+        branch4.Add(branch7);
+        Branch branch8 = new Branch();
+        branch5.Add(branch8);
+        Branch branch9 = new Branch();
+        branch5.Add(branch9);
+        Branch branch10 = new Branch();
+        branch8.Add(branch10);
+
+        int depth = root.GetDepth(root);
+        Console.WriteLine($"Level of branch branch10: {depth}");
+
+        //idk why I cant get all the branches I added to the branches list...
+        Console.WriteLine(root.Show());
+    }
+}
